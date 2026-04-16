5cf0b0df93cec84a4395e8fbaad1c35912a80b18
diegovalenzueladk <diegovalenzueladk@gmail.com>
2026-04-15 22:43:52 -0300

    Ejercicio_Logica

---
+++ Practica Logica/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace PracticaLogica;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int cantidad = 20;
+        string[] nombres = new string[cantidad];
+        int[] edades = new int[cantidad];
+
+        // Pedir datos
+        Console.WriteLine("-- INGRESE LOS SIGUIENTE "+cantidad+" NOMBRES Y EDADES --");
+        for (int i = 0; i < cantidad; i++)
+        {
+            Console.Write("Nombre "+i+": ");
+            nombres[i] = Console.ReadLine();
+
+            Console.Write("Edad "+i+": ");
+            edades[i] = int.Parse(Console.ReadLine());
+            Console.WriteLine("------------------------");
+        }
+
+        // Mostrar resultados
+        Console.WriteLine("\n---DATOS INGRESADOS---");
+        for (int i = 0; i < cantidad; i++)
+        {
+            Console.Write(i+" _"+nombres[i] + " tiene " + edades[i] + " años - ");
+
+            if (edades[i] > 30)
+                Console.WriteLine("Puede Ingresar");
+            else
+                Console.WriteLine("No puede ingresar, vuelva a casa.");
+        }
+        Console.WriteLine("Precione ENTER para Salir...");
+        Console.ReadLine();
+    }
+}
